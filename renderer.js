1dbace2075a95241a438d8bd2000e432463fbc8d
msnetkov <msnetkov@sfu-kras.ru>
2017-06-15 16:57:47 +0700

    Переход на Semantic UI, набросок части интерфейса

--- renderer.js
+++ renderer.js
@@ -1,3 +1,66 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+/* global $, Plotly */
+const Mustache = require('Mustache')
+const {Task, Point, Edge} = require('./classes.js')
+const ipc = require('electron').ipcRenderer
+
+const $edges = $('#edges')
+const $edgesCount = $('#edges-count')
+
+let gd
+let task = new Task()
+
+$(document).ready(function () {
+  task.addEdge(new Edge(new Point(1, 0), new Point(2, 2)))
+
+  $edges.on('click', 'a', function (e) {
+    e.preventDefault()
+
+    let $el = $(this)
+
+    task.removeByHash($el.data('hash'))
+    $el.remove()
+    syncEdgeChanges()
+  })
+
+  syncEdgeChanges()
+
+  let trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    type: 'scatter'
+  }
+
+  let trace2 = {
+    x: [1, 2, 3, 4],
+    y: [16, 5, 11, 9],
+    type: 'scatter'
+  }
+
+  let data = [trace1, trace2]
+  let gd3 = Plotly.d3.select('#plot')
+
+  gd = gd3.node()
+
+  let layout = {
+    title: 'Планарный граф'
+  }
+
+  let options = {
+    staticPlot: true
+  }
+
+  Plotly.plot(gd, data, layout, options)
+})
+
+window.onresize = function () {
+  Plotly.Plots.resize(gd)
+}
+
+function syncEdgeChanges () {
+  $edges.empty()
+  $edgesCount.text(task.edges.length)
+
+  for (let edge of task.edges) {
+    $edges.append(Mustache.render($('#template-edge-label').html(), edge.objWithHash))
+  }
+}
